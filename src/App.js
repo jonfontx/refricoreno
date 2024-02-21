5d4a11cc8271bf8c1aee42f46a5eb0342859fa72
jonfontx <Your GitHub Email Address>
2024-02-21 18:32:26 +0000

    third

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ import Homepage from './Components/Homepage';
 
 
 const App = () => {
-  const [availableTimes, setAvailableTimes] = useState([
-    '17:00',
-    '18:00',
-    '19:00',
-    '20:00',
-    '21:00',
-    '22:00',
-  ]);
+
 
   return (
     <BrowserRouter>
