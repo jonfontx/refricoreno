553908a55e38c6243d15074e272dc887339c7a5b
jonfontx <Your GitHub Email Address>
2024-02-22 16:21:45 +0000

    fix2

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -1,5 +1,4 @@
 import React from "react";
-import restohead from "../icons_assets/restauranfood.jpg";
 import {Link} from 'react-router-dom';
 import video from '../icons_assets/tecnico1.webm';
 
@@ -18,7 +17,7 @@ const CallToAction = () => {
                 <br></br> Todas las marcas. Atención eficiente y eficaz. En el menor tiempo y al menor costo. Trabajos garantizados por 3 meses. 30 años de experiencia avalan nuestros trabajos. Operamos en Málaga Capital y Costa del Sol</p>
                 <button onClick={handleClick} aria-label="On Click"><Link to ='BookingPage'>951 15 16 42</Link></button>
                 </div>
-            <video className='videoTag' autoPlay="autoPlay" loop="loop" muted="muted" playsInline="playsInline" style={{position:"absolute", zIndex:0}}>
+            <video className='videoTag' autoPlay="autoPlay" loop="loop" muted="muted" playsInline="playsInline">
                 <source src={video} type='video/webm' />
                 </video>
         </section>
