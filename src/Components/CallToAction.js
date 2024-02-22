c651149d0dd854bd5695e8ed12860d1a10a83a5f
jonfontx <Your GitHub Email Address>
2024-02-22 15:59:06 +0000

    callaction

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -11,10 +11,10 @@ const CallToAction = () => {
     <header>
         <section>
             <div>
-                <h2>Little Lemon</h2>
-                <h3>Chicago</h3>
-                <p>Discover the bright and zesty flavors of Little Lemon.
-                <br></br> Locally sourced ingredients, expertly crafted dishes, and a cozy ambiance that will make you feel right at home</p>
+                <h2>Refrigeración Coreno</h2>
+                <h3>951 15 16 42</h3>
+                <p>Servicio Técnico especializado en neveras y congeladores familiares y comerciales.
+                <br></br> Todas las marcas. Atención eficiente y eficaz. En el menor tiempo y al menor costo. Trabajos garantizados por 3 meses. 30 años de experiencia avalan nuestros trabajos. Operamos en Málaga Capital y Costa del Sol</p>
                 <button onClick={handleClick} aria-label="On Click"><Link to ='BookingPage'>Reserve a Table</Link></button>
                 </div>
             <img src={restohead} alt="food"/>
