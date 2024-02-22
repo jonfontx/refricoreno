73fd89dbd86227bd37bcfe8bd651b21382cfed63
jonfontx <Your GitHub Email Address>
2024-02-22 16:05:45 +0000

    fix1

--- src/Components/CallToAction.js
+++ src/Components/CallToAction.js
@@ -14,8 +14,8 @@ const CallToAction = () => {
                 <h2>Refrigeración Coreno</h2>
                 <h3>Servicio Técnico</h3>
                 <p>Especializado en neveras y congeladores familiares y comerciales.
-                <br></br> Todas las marcas. Atención eficiente y eficaz. En el menor tiempo y al menor costo. Trabajos garantizados por 3 meses. 30 años de experiencia avalan nuestros trabajos. Operamos en Málaga Capital y Costa del Sol</p>
-                <button onClick={handleClick} aria-label="On Click"><Link to ='BookingPage'>Reserve a Table</Link></button>
+                <br></br> Todas las marcas. Atención eficiente y eficaz. En el menor tiempo y al menor costo. <br></br> Trabajos garantizados por 3 meses. 30 años de experiencia avalan nuestros trabajos.<br></br> Operamos en Málaga Capital y Costa del Sol</p>
+                <button onClick={handleClick} aria-label="On Click"><Link to ='BookingPage'>951 15 16 42</Link></button>
                 </div>
             <img src={restohead} alt="food"/>
         </section>
